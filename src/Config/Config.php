531d50811867d8c19bbed3c0571faf08767c43d8
吴蒙 <wumeng@gupo.onaliyun.com>
2023-06-30 16:10:43 +0800

    修改Config

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -2,6 +2,10 @@
 
 namespace Gupo\MiddleOffice\Config;
 
+use Gupo\MiddleOffice\Utils\Utils;
+use Gupo\MiddleOffice\Error\ErrorInfo;
+use Gupo\MiddleOffice\Exception\ClientException;
+
 /**
  * Class Config
  *
@@ -12,10 +16,37 @@ class Config
 {
     public string $accessKey;
     public string $accessSecret;
+    public string $appId;
 
-    public function __construct($accessKey, $accessSecret)
+    public function __construct()
     {
-        $this->accessKey = $accessKey;
-        $this->accessSecret = $accessSecret;
+        $this->init();
+    }
+
+    public function init()
+    {
+        $outerConfigPath = __DIR__ . '/../../../../../config/middleoffice.php';
+        $innerConfigPath = __DIR__ . '/configfile.php';
+        if (!file_exists($outerConfigPath)) {
+            $this->appId = $_ENV['AUTH_CENTER_ID'] ?? 0;
+            $this->accessKey = $_ENV['AUTH_CENTER_APP_ID'] ?? '';
+            $this->accessSecret = $_ENV['AUTH_CENTER_APP_SECRET'] ?? '';
+        } else {
+            // 将该配置文件复制到当前目录下的configfile中
+            copy($outerConfigPath, $innerConfigPath);
+            $config = include $innerConfigPath;
+            if (Utils::empty_($config)) {
+                throw new ClientException(ErrorInfo::CONFIG_ERROR."1");
+            }
+            if (Utils::isUnset($config['id'])
+                || Utils::isUnset($config['app_id'])
+                || Utils::isUnset($config['app_secret'])
+            ) {
+                throw new ClientException(ErrorInfo::CONFIG_ERROR."2");
+            }
+            $this->appId = $config['id'];
+            $this->accessKey = $config['app_id'];
+            $this->accessSecret = $config['app_secret'];
+        }
     }
 }
